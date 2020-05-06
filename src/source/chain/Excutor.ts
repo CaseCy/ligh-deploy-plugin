19a29965dbda661d7448b4a2f0c85cbc3379c4c6
Cc <cononyc@163.com>
2020-05-06 11:08:28 +0800

    小bug修复

--- src/source/chain/Excutor.ts
+++ src/source/chain/Excutor.ts
@@ -19,8 +19,9 @@ export class BuildExcutor implements Excutor {
         const { local, build } = context.activeConfig;
         buildConfig.path = local.projectRootPath;
         local.buildCmdExePath ? buildConfig.path = local.buildCmdExePath : "";
-        build ? Object.assign(buildConfig, build) : context.activeConfig.build = buildConfig;
+        build ? Object.assign(buildConfig, build) : "";
         Log.log("开始进行项目构建，构建路径：" + buildConfig.path + " 执行的命令：" + buildConfig.cmd)
+        context.activeConfig.build = buildConfig
         const { err, stdout, } = await projectBuild.build(buildConfig);
         if (err) {
             throw err;
